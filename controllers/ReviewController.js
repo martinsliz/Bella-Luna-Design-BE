2983423b0f78e2a5134069d75748510d8757fe2f
Liz Martins <elizabeth.martins5@gmail.com>
2023-03-27 09:12:51 -0400

    review controller

---
+++ controllers/ReviewController.js
@@ -0,0 +1,66 @@
+const { Review } = require('../models')
+
+const GetReviews = async (req, res) => {
+  try {
+    const reviews = await Review.findAll({ order: [['createdAt', 'DESC']] })
+    res.send(reviews)
+  } catch (error) {
+    throw error
+  }
+}
+
+const GetReviewDetails = async (req, res) => {
+  try {
+    const review = await Review.findByPk(req.params.review_id)
+    res.send(review)
+  } catch (error) {
+    throw error
+  }
+}
+
+const CreateReview = async (req, res) => {
+  try {
+    let userId = parseInt(req.params.user_id)
+    let productId = parseInt(req.params.product_id)
+    let reviewBody = {
+      userId,
+      productId,
+      ...req.body
+    }
+    let review = await Review.create(reviewBody)
+    res.send(review)
+  } catch (error) {
+    throw error
+  }
+}
+
+const UpdateReview = async (req, res) => {
+  try {
+    let reviewId = parseInt(req.params.review_id)
+    let updatedReview = await Review.update(req.body, {
+      where: { id: reviewId },
+      returning: true
+    })
+    res.send(updatedReview)
+  } catch (error) {
+    throw error
+  }
+}
+
+const DeleteReview = async (req, res) => {
+  try {
+    let reviewId = parseInt(req.params.review_id)
+    await Review.destroy({ where: { id: reviewId } })
+    res.send({ message: `Deleted review with an id of ${reviewId}` })
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  GetReviews,
+  GetReviewDetails,
+  CreateReview,
+  UpdateReview,
+  DeleteReview
+}
