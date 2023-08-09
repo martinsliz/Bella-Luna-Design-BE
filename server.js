70c4317a8ae5c1121d2dc291b2a5bec8058e4975
Liz Martins <elizabeth.martins5@gmail.com>
2023-08-08 21:15:45 -0400

    prep for etsy api

--- server.js
+++ server.js
@@ -12,5 +12,27 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.json({ message: 'Bella Luna Designs!!!' }))
+
+app.get('/ping', async (req, res) => {
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      'x-api-key': 'n0u0ygip0pqtytzx8mmizw78'
+    }
+  }
+
+  const response = await fetch(
+    'https://api.etsy.com/v3/application/openapi-ping',
+    requestOptions
+  )
+
+  if (response.ok) {
+    const data = await response.json()
+    res.send(data)
+  } else {
+    res.send('oops')
+  }
+})
+
 app.use('/api', AppRouter)
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
